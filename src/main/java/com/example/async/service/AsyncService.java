444caedc916cd7210a419c72d51d6638c79d6588
iian <yiaahn@gmail.com>
2023-11-05 20:26:07 +0900

    exec-1 & task-1 동작 확인

--- src/main/java/com/example/async/service/AsyncService.java
+++ src/main/java/com/example/async/service/AsyncService.java
@@ -11,6 +11,7 @@ public class AsyncService {
     @Async // 비동기로 동작
     public void hello() {
 
+        // 이렇게 코딩하진않지만 thread 확인용
         for (int i=0; i<10; i++){
             try {
                 Thread.sleep(2000);
